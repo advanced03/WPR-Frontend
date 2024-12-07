8616c119b15110b08e57eda3202aab4a70aed313
advanced03 <incedalsrf@gmail.com>
2024-12-07 19:08:43 +0100

    beetje meer styling gegeven

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import '../style/home.css';
 
@@ -11,50 +11,49 @@ const Home = () => {
   };
 
   return (
-    <Container className="home-container">
+    <>
       <div className="home-titel">
         <h1>Welkom bij Car And All!</h1>
       </div>
-
-      <Row className="justify-content-center my-4">
-        <Col className="mb-4">
+      <Row className="justify-content-center foto-rij">
+        <Col>
           <div className="home-image-container">
             <img src="src/assets/auto.png" className="img-fluid" alt="Auto" />
           </div>
         </Col>
-        <Col className="mb-4">
+        <Col>
           <div className="home-image-container">
             <img src="src/assets/caravan.png" className="img-fluid" alt="Caravan" />
           </div>
         </Col>
-        <Col className="mb-4">
+        <Col>
           <div className="home-image-container">
             <img src="src/assets/camper.png" className="img-fluid" alt="Camper" />
           </div>
         </Col>
       </Row>
 
-      <div className="text-center my-4">
+      <div className="text-center">
         <Button
           variant="primary"
-          className="mx-2 home-btn"
+          className="mx-2 btn-lg home-btn"
           onClick={() => handleNavigation('/Login')}
         >
           Login
         </Button>
         <Button
           variant="primary"
-          className="mx-2 home-btn"
+          className="mx-2 my-5 btn-lg home-btn"
           onClick={() => handleNavigation('/ZakelijkRegister')}
         >
           Medewerker
         </Button>
       </div>
 
-      <div className="text-center my-4">
+      <div className="text-center home-inhoud">
         <p>Huur auto's, caravans en campers! Bij Car And All hebben we een breed scala aan voertuigen voor een aantrekkelijke huurprijs. Wilt u een voertuig huren? Maak dan snel een account en begin uw rij avontuur!</p>
       </div>
-    </Container>
+    </>
   );
 }
 
