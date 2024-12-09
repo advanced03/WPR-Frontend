d8667963cb160d987294c12c1a36b9ca2f51dbe1
advanced03 <incedalsrf@gmail.com>
2024-12-09 19:06:45 +0100

    verbeteringen aan de home pagina

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,6 @@ import auto from '../assets/auto.png';
 import caravan from '../assets/caravan.png';
 import camper from '../assets/camper.png';
 
-
 const Home = () => {
   const navigate = useNavigate();
 
@@ -16,6 +15,7 @@ const Home = () => {
 
   return (
     <>
+      <div className='achtergrond2'></div>
       <div className="home-titel">
         <h1>Welkom bij Car And All!</h1>
       </div>
@@ -28,19 +28,19 @@ const Home = () => {
         </Col>
         <Col>
           <div className="home-image-container">
-          <h3 className='HomeFotoTekst'>Caravans</h3>
+            <h3 className='HomeFotoTekst'>Caravans</h3>
             <img src={caravan} className="img-fluid" alt="Caravan" />
           </div>
         </Col>
         <Col>
           <div className="home-image-container">
-          <h3 className='HomeFotoTekst'>Campers</h3>
+            <h3 className='HomeFotoTekst'>Campers</h3>
             <img src={camper} className="img-fluid" alt="Camper" />
           </div>
         </Col>
       </Row>
 
-      <div className="text-center">
+      <div className="button-div text-center">
         <Button
           className="mx-2 btn-lg home-btn"
           onClick={() => handleNavigation('/Login')}
@@ -49,25 +49,44 @@ const Home = () => {
         </Button>
         <Button
           className="btn-lg home-btn"
-          onClick={() => handleNavigation('/ZakelijkRegister')}>
+          onClick={() => handleNavigation('/ZakelijkRegister')}
+        >
           Medewerker
         </Button>
+
+        <div className="text-center mt-3">
+          <span>
+            Werkt u voor Car And All?{' '}
+            <button
+              onClick={() => handleNavigation('/ZakelijkRegister')}
+              className="Link"
+            >
+              Klik hier
+            </button>{' '}
+            om u aan te melden voor een werknemer account.
+          </span>
+        </div>
       </div>
-      <div className="text-center">
-        <span>Werkt u voor Car And All? <button onClick={() => handleNavigation('/ZakelijkRegister')} className="Link">Klik hier</button> om u aan te melden voor een werknemer account.</span>
-      </div>
+
       <div className="text-center home-inhoud">
-        <p>
-          Welkom bij Car And All! Wij bieden een uitgebreid assortiment auto's, caravans en campers tegen scherpe huurprijzen.
-          Of je nu een weekendtrip plant of een langere reis, wij hebben het perfecte voertuig voor jou.
-          Maak vandaag nog een account aan en begin jouw avontuur op de weg!
-        </p>
-        <h3>Waarom kiezen voor Car And All?</h3>
-        <p>
-          ✔ Groot aanbod aan voertuigen<br />
-          ✔ Flexibele en transparante voorwaarden<br />
-          ✔ Altijd hulp van onze vriendelijke klantenservice
-        </p>
+        <Row>
+          <Col md={6}>
+          <h3>Over ons</h3>
+            <p>
+              Welkom bij Car And All! Wij bieden een uitgebreid assortiment auto's, caravans en campers tegen scherpe huurprijzen.
+              Of je nu een weekendtrip plant of een langere reis, wij hebben het perfecte voertuig voor jou.
+              Maak vandaag nog een account aan en begin jouw avontuur op de weg!
+            </p>
+          </Col>
+          <Col md={6}>
+            <h3>Waarom kiezen voor Car And All?</h3>
+            <ul className="list-unstyled">
+              <li>✔ Groot aanbod aan voertuigen</li>
+              <li>✔ Flexibele en transparante voorwaarden</li>
+              <li>✔  Altijd hulp van onze vriendelijke klantenservice</li>
+            </ul>
+          </Col>
+        </Row>
       </div>
 
     </>
